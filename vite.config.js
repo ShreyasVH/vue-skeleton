60c4ce337867dbc5467d75985877e0468c1a14c5
Shreyas Hande <shreyas@everwell.org>
2022-10-26 20:54:15 +0530

    moved out https

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -17,11 +16,7 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       host: process.env.VITE_HOST,
-      port: process.env.VITE_PORT,
-      https: {
-        key: fs.readFileSync(process.env.VITE_SSL_KEY_FILE),
-        cert: fs.readFileSync(process.env.VITE_SSL_CRT_FILE)
-      }
+      port: process.env.VITE_PORT
     }
   }
 })
